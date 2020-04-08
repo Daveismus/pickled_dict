4fe65c50522480aa20c58264715ae21396cc6933
Daveismus <55088389+Daveismus@users.noreply.github.com>
2020-04-08 11:38:24 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(name='pickled_dict',  # How you named your package folder (MyLib)
       packages=['pickled_dict'],  # Chose the same as "name"
-      version='0.1',  # Start with a small number and increase it with every change you make
+      version='0.1.5',  # Start with a small number and increase it with every change you make
       license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
       description='Have a persistant Dictionary saved as file',  # Give a short description about your library
       author='David Brielbeck',  # Type in your name
